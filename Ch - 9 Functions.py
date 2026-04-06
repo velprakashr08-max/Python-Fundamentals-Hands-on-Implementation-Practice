0d405bf18316d007ccc863d230189ad55e5ff2a4
Vel Prakash <velprakashr08@gmail.com>
2026-04-06 13:00:45 +0530

    feat: implement functions with parameters (Chapter 9)

---
+++ Ch - 9 Functions.py
@@ -0,0 +1,17 @@
+def say_hi():
+    print("Hello World")
+print("Top")
+say_hi()
+print("Bottom")
+
+def say_hello(name):
+    print("Hello " + name)
+say_hello("Jack")
+
+def say_hey(name, age):
+    print("Hello " + name + ", you are " + age)
+say_hey("Jeff", "25")
+
+def say_bye(name, age):
+    print("Hello " + name + ", you are " + str(age))
+say_bye("Jim", 20)
