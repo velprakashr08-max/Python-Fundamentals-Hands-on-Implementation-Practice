bf0277f1274ef99e7e8287225b691803e15742b2
Vel Prakash <velprakashr08@gmail.com>
2026-04-08 14:19:10 +0530

    feat: add conditional logic using if-elif-else (Chapter 11)

--- Ch - 11 If Statements.py
+++ Ch - 11 If Statements.py
@@ -42,3 +42,54 @@ if is_male and is_tall:
     print("You are a tall male")
 else:
     print("You are either not a male or tall or both")
+
+is_male = False
+is_tall = False
+if is_male and is_tall:
+    print("You are a tall male")
+else:
+    print("You are either not a male or tall or both")
+
+is_male = True
+is_tall = True
+if is_male and is_tall:
+    print("You are a tall male")
+elif is_male and not(is_tall):
+    print("You are a short male")
+elif not(is_male) and is_male:
+    print("You are not a male but are tall")
+else:
+    print("You not a male and not tall")
+
+is_male = False
+is_tall = True
+if is_male and is_tall:
+    print("You are a tall male")
+elif is_male and not(is_tall):
+    print("You are a male but are not tall")
+elif not(is_male) and is_tall:
+    print("You are not a male but are tall")
+else:
+    print("You are not a male and not tall")
+
+is_male = True
+is_tall = False
+if is_male and is_tall:
+    print("You are a tall male")
+elif is_male and not(is_tall):
+    print("You are a male but are not tall")
+elif not(is_male) and is_tall:
+    print("You are not a male but are tall")
+else:
+    print("You are not a male and not tall")
+
+is_male = False
+is_tall = False
+if is_male and is_tall:
+    print("You are a tall male")
+elif is_male and not(is_tall):
+    print("You are a male but are not tall")
+elif not(is_male) and is_tall:
+    print("You are not a male but are tall")
+else:
+    print("You are not a male and not tall")
