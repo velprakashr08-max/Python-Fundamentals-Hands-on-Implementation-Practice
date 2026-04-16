1b5b049b241c02ac7859dce2315c8b2cd10c068b
Vel Prakash <velprakashr08@gmail.com>
2026-04-16 19:35:56 +0530

    feat: build text translator using conditional and loop logic (Chapter - 21)

--- Ch - 21 Building a Translator.py
+++ Ch - 21 Building a Translator.py
@@ -1,8 +1,12 @@
 def translate(phrase):
     translation = ""
     for letter in phrase:
-        if letter in "AEIOUaeiou":
-            translation = translation + "g"
+        if letter.lower() in "aeiou":
+            if letter.upper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+    
         else:
             translation = translation + letter
     return translation
