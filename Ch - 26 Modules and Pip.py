120e78e556f270443e9a214acaa7518ba9c1c967
Vel Prakash <velprakashr08@gmail.com>
2026-04-25 14:46:53 +0530

    feat: add modules and pip basics (Chapter - 26)

---
+++ Ch - 26 Modules and Pip.py
@@ -0,0 +1,9 @@
+import Ch_26_useful_tools
+
+print(Ch_26_useful_tools.roll_dice(10))
+
+print(Ch_26_useful_tools.beatles)
+
+print(Ch_26_useful_tools.meters_in_kilometer)
+
+print(Ch_26_useful_tools.feet_in_mile)
