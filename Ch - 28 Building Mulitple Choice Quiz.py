1418200fe2cc37cd675947d76ee608fd610ba494
Vel Prakash <velprakashr08@gmail.com>
2026-04-27 22:34:19 +0530

    feat: implement guessing game (Chapter 28)

---
+++ Ch - 28 Building Mulitple Choice Quiz.py
@@ -0,0 +1,5 @@
+question_prompts = [
+    "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+    "What color are Bananas?\n(a) Teal\n(b) Magenta\n(c)Yellown\n",
+    "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+]
